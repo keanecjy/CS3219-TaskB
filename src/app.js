5d32ab9b17c538dca788b8a2261d3ad886081fd0
Keane <e0425601@u.nus.edu>
2021-09-29 20:33:12 +0800

    update app config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+var logger = require('morgan');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var indexRouter = require('./routes/index');
+var app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/v1', indexRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  return res.status(200).json({ message: 'Welcome to Express API template' });
 });
-
 module.exports = router;
