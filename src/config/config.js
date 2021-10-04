f2e77c2b91a9e92e2cfb9fc7496afbfef8857a72
Keane <e0425601@u.nus.edu>
2021-10-05 00:27:02 +0800

    use database url

--- src/config/config.js
+++ src/config/config.js
@@ -23,7 +23,7 @@ module.exports = {
   },
 
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
