d5c2c396da8d2b127067679fd518b94bd316f96e
Keane <e0425601@u.nus.edu>
2021-10-05 13:05:36 +0800

    Add cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import router from './routes/index';
+import cors from 'cors';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/movies', router);
 
