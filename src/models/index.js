9379b74a398353e47c7620543f4bf855f1376ddc
Keane <e0425601@u.nus.edu>
2021-10-05 10:12:41 +0800

    update sequelize

--- src/models/index.js
+++ src/models/index.js
@@ -16,27 +16,8 @@ let sequelize;
 if (env === 'production') {
   console.log("Starting sequelize");
   sequelize = new Sequelize(process.env.DATABASE_URL, config);
-
-  console.log(sequelize);
-
-  // sequelize = new Sequelize(
-  //   process.env.DB_NAME,
-  //   process.env.DB_USER,
-  //   process.env.DB_PASS,
-  //   {
-  //     host: process.env.DB_HOST,
-  //     port: process.env.DB_PORT,
-  //     dialect: 'postgres',
-  //     dialectOption: {
-  //       ssl: true,
-  //       native: true,
-  //     },
-  //     logging: true,
-  //   }
-  // );
-  // console.log(sequelize);
 } else {
-  console.log("Starting default database creation");
+  console.log("Starting local database creation");
   sequelize = new Sequelize(
     config.database,
     config.username,
